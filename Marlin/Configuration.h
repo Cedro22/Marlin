511988565aefc9b28074aaa7f29b623b13486a00
Cédric <58825787+Cedro22@users.noreply.github.com>
2023-04-02 00:13:44 +0200

    Updating preheat temperatures for PLA and PETG

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2196,13 +2196,13 @@
 // Preheat Constants - Up to 6 are supported without changes
 //
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 205 
-#define PREHEAT_1_TEMP_BED     60
+#define PREHEAT_1_TEMP_HOTEND 215 
+#define PREHEAT_1_TEMP_BED     50
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "PETG"
 #define PREHEAT_2_TEMP_HOTEND 235
-#define PREHEAT_2_TEMP_BED    80
+#define PREHEAT_2_TEMP_BED    70
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
 // @section motion
